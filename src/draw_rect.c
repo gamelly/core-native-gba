40b3e9b34faf73ead3ed3e5b6175e76abc469861
RodrigoDornelles <rodrigo@dornelles.me>
2024-10-12 14:09:50 -0300

    feat: mode 1 in draw rect

--- src/draw_rect.c
+++ src/draw_rect.c
@@ -8,6 +8,22 @@ void native_draw_rect_flush()
     uint16_t w = draw_queue[draw_index_erase++];
     uint16_t h = draw_queue[draw_index_erase++];
 
+    if (mode) {
+        for (int i = 0; i < w; i++) {
+            *(uint16_t *)(0x06000000 + ((y) * 240 + (x + i)) * 2) = color_current.pixel;
+        }
+        for (int i = 0; i <= w; i++) {
+            *(uint16_t *)(0x06000000 + ((y + h) * 240 + (x + i)) * 2) = color_current.pixel;
+        }
+        for (int i = 0; i < h; i++) {
+            *(uint16_t *)(0x06000000 + ((y + i) * 240 + (x)) * 2) = color_current.pixel;
+        }
+        for (int i = 0; i < h; i++) {
+            *(uint16_t *)(0x06000000 + ((y + i) * 240 + (x + w)) * 2) = color_current.pixel;
+        }
+        return;
+    }
+
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
             *(uint16_t *)(0x06000000 + ((y + i) * 240 + (x + j)) * 2) = color_current.pixel;
