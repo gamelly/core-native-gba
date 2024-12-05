c5dc0f585867172cfc717f70147ccf1acf231a02
RodrigoDornelles <rodrigo@dornelles.me>
2024-12-05 14:41:22 -0300

    fix: change background color

--- src/draw_color.c
+++ src/draw_color.c
@@ -20,8 +20,17 @@ void draw_cmd_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
     }
 
     if (flush_mode) {
+        static const uint32_t *end = (uint32_t *) (0x06000000 + (240 * 160 * 2));
+        static uint16_t old_clear = 0;
         draw_color.arr[0] = clear.pixel;
         draw_color.arr[1] = clear.pixel;
+        if (old_clear != clear.pixel) {
+            uint32_t *i = (uint32_t *) 0x06000000;
+            while (i < end) {
+                *i++ = draw_color.pixel2;
+            }
+            old_clear = clear.pixel;
+        }
     } else {
         draw_color.arr[0] = tint.pixel;
         draw_color.arr[1] = tint.pixel;
