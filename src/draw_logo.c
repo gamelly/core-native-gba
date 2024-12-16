c6fe1cf801395070e8e0af6b8161947e5fbb8162
RodrigoDornelles <rodrigo@dornelles.me>
2024-12-16 16:32:01 -0300

    feat: new logos for love and core native

--- src/draw_logo.c
+++ src/draw_logo.c
@@ -8,19 +8,40 @@ extern void gly_type_render(uint8_t, uint8_t, uint8_t, char*, void*);
 
 void draw_logo()
 {
-#if ENGINE_LOGO != 0
+#if ENGINE_LOGO > 0
     draw_queue_page(1);
     draw_queue_push(49, 0xFF, 0xFF, 0xFF, 0);
     draw_queue_push(48, 3, 0, 1, 0);
     draw_queue_push(49, 0, 0, 0, 0);
 #if ENGINE_LOGO == 1
-    draw_queue_push(50, 90, 45, 50, 45);
+    static const uint8_t ver_x = 97;
+    static const uint8_t ver_y = 80;
     gly_type_render(95, 60, 12, "gly", draw_queue_clojure(51));
-#else
-    gly_type_render(95, 67, 5, "native", draw_queue_clojure(51));
-#endif
     gly_type_render(95, 74, 5, "engine", draw_queue_clojure(51));
-    gly_type_render(97, 80, 4, "0.0.17", draw_queue_clojure(51));
+    draw_queue_push(50, 90, 45, 50, 45);
+#elif ENGINE_LOGO == 2
+    static const uint8_t ver_x = 97;
+    static const uint8_t ver_y = 84;
+    draw_queue_push(48, 0, 0, 1, 0);
+    draw_queue_push(49, 0x80, 0xC0, 0xFF, 0);
+    draw_queue_push(50, 90, 45, 50, 22);
+    draw_queue_push(49, 0xFF, 0x80, 0x80, 0);
+    draw_queue_push(50, 90, 45+22, 50, 23);
+    draw_queue_push(48, 1, 0, 1, 0);
+    draw_queue_push(49, 0xFF, 0xFF, 0xFF, 0);
+    gly_type_render(95, 67-5, 9, "LOVE", draw_queue_clojure(51));
+    draw_queue_push(51, 90+40, 45+2, 90+48, 45+2);
+    draw_queue_push(51, 90+48, 45+2, 90+48, 45+6);
+    draw_queue_push(50, 90, 45, 50, 45);
+#elif ENGINE_LOGO == 3
+    static const uint8_t ver_x = 107;
+    static const uint8_t ver_y = 80;
+    gly_type_render(95, 50, 5, "GameBoy", draw_queue_clojure(51));
+    gly_type_render(95, 56, 4, "advanced", draw_queue_clojure(51));
+    gly_type_render(95, 62, 10, "CORE", draw_queue_clojure(51));
+    gly_type_render(95, 74, 5, "native", draw_queue_clojure(51));
+#endif
+    gly_type_render(ver_x, ver_y, 4, "0.0.17", draw_queue_clojure(51));
     draw_queue_burn(1);
 #endif
 }
