e9e71d6a7a97aa1c085264b77ad80458f984d24e
RodrigoDornelles <rodrigo@dornelles.me>
2025-02-14 16:39:25 -0300

    feat: revision 0.0.18 (#11)

--- src/draw_logo.c
+++ src/draw_logo.c
@@ -16,7 +16,7 @@ void draw_logo()
     draw_queue_push(48, 3, 0, 1, 0);
     draw_queue_push(49, 0, 0, 0, 0);
 #if ENGINE_LOGO == 1
-    static const uint8_t ver_x = 97;
+    static const uint8_t ver_x = 95;
     static const uint8_t ver_y = 80;
     draw_queue_push(48, 0, 0, 1, 0);
     draw_queue_push(49, 0x00, 0x00, 0xFF, 0);
@@ -33,11 +33,11 @@ void draw_logo()
     draw_queue_push(50, 90, 45+35, 50, 10);
     draw_queue_push(48, 1, 0, 1, 0);
     draw_queue_push(49, 0xFF, 0xFF, 0xFF, 0);
-    gly_type_render(95, 56, 12, "gly", draw_queue_clojure(51));
-    gly_type_render(95, 72, 5, "engine", draw_queue_clojure(51));
+    gly_type_render(95, 56, 12, "GLY", draw_queue_clojure(51));
+    gly_type_render(95, 72, 5, "ENGINE", draw_queue_clojure(51));
     draw_queue_push(50, 90, 45, 50, 45);
 #elif ENGINE_LOGO == 2
-    static const uint8_t ver_x = 97;
+    static const uint8_t ver_x = 96;
     static const uint8_t ver_y = 84;
     draw_queue_push(48, 0, 0, 1, 0);
     draw_queue_push(49, 0x80, 0xC0, 0xFF, 0);
@@ -51,14 +51,14 @@ void draw_logo()
     draw_queue_push(51, 90+48, 45+2, 90+48, 45+6);
     draw_queue_push(50, 90, 45, 50, 45);
 #elif ENGINE_LOGO == 3
-    static const uint8_t ver_x = 107;
+    static const uint8_t ver_x = 114;
     static const uint8_t ver_y = 80;
-    gly_type_render(95, 50, 5, "GameBoy", draw_queue_clojure(51));
-    gly_type_render(95, 56, 4, "advanced", draw_queue_clojure(51));
-    gly_type_render(95, 62, 10, "CORE", draw_queue_clojure(51));
-    gly_type_render(95, 74, 5, "native", draw_queue_clojure(51));
+    gly_type_render(95, 45, 7, "GameBoy", draw_queue_clojure(51));
+    gly_type_render(95, 53, 6, "Advanced", draw_queue_clojure(51));
+    gly_type_render(95, 60, 13, "CORE", draw_queue_clojure(51));
+    gly_type_render(95, 74, 5, "Native", draw_queue_clojure(51));
 #endif
-    gly_type_render(ver_x, ver_y, 4, "0.0.17", draw_queue_clojure(51));
+    gly_type_render(ver_x, ver_y, 5, "0.0.18", draw_queue_clojure(51));
     draw_queue_burn(1);
 #endif
 }
